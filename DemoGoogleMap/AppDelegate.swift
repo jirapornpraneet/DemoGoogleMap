62b40033716aa9d533f7f6785f99f1a6f7667c44
Piesoft <nut.jirapornpraneet@gmail.com>
2023-01-18 11:11:58 +0700

    add initial google map

--- DemoGoogleMap/AppDelegate.swift
+++ DemoGoogleMap/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyBkduNTeiSMudo-bbdddMqpg9Y3SNk0M34")
         return true
     }
 
